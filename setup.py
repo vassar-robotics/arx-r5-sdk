a895e02e07366ee15ba39b109295653a33b66fe6
Charles Yong <charles@vassarrobotics.com>
2025-08-06 17:17:29 -0700

    Fix PyPI upload by only distributing source packages
    - Update version to 0.2.1 in all files
    - Change workflow to only build sdist, not bdist_wheel
    - PyPI doesn't accept linux_x86_64 platform tags
    - Users will compile from source when installing

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="vassar-arx-r5-sdk",
-    version="0.2.0",
+    version="0.2.1",
     author="ARXrobotics",
     author_email="contact@arx-x.com",
     maintainer="Vassar",
