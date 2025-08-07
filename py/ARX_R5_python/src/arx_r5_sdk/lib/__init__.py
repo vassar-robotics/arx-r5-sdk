99ccde97124d1a726a09967990c177216ff1e763
Charles Yong <charles@vassarrobotics.com>
2025-08-06 19:43:41 -0700

    fix: Use LD_LIBRARY_PATH to load shared libraries

--- py/ARX_R5_python/src/arx_r5_sdk/lib/__init__.py
+++ py/ARX_R5_python/src/arx_r5_sdk/lib/__init__.py
@@ -1,19 +1,14 @@
 import os
-import ctypes
 
 # Get the absolute path to the directory containing this file
 _lib_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Add this directory to the dynamic linker's search path
+# Prepend our library directory to LD_LIBRARY_PATH
 # This is for Linux. A similar approach would be needed for other OSes.
-try:
-    # This is a bit of a hack, but it's the most reliable way to do this
-    # without requiring the user to set LD_LIBRARY_PATH.
-    ctypes.CDLL(f'libdl.so.2').dlopen(_lib_dir, ctypes.RTLD_GLOBAL)
-except (OSError, AttributeError):
-    # If dlopen is not available, we can't do much.
-    # This will likely fail on non-Linux systems.
-    pass
+if "LD_LIBRARY_PATH" in os.environ:
+    os.environ["LD_LIBRARY_PATH"] = f"{_lib_dir}:{os.environ['LD_LIBRARY_PATH']}"
+else:
+    os.environ["LD_LIBRARY_PATH"] = _lib_dir
 
 # Now we can import the modules
 from . import arx_r5_python
