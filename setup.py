37714b60cc8910e7268724c989fc1b3f323df7f0
Charles Yong <charles@vassarrobotics.com>
2025-08-06 21:24:51 -0700

    fix: Copy shared libraries to build directory

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ class CMakeExtension(Extension):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
+import shutil
+
 class CMakeBuild(build_ext):
     def run(self):
         try:
@@ -37,6 +39,11 @@ class CMakeBuild(build_ext):
         
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_dir)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=build_dir)
+
+        # Copy the pre-compiled .so files to the same directory as the compiled extensions
+        for so_file in os.listdir('py/ARX_R5_python/src/arx_r5_sdk/lib'):
+            if so_file.endswith('.so'):
+                shutil.copy(os.path.join('py/ARX_R5_python/src/arx_r5_sdk/lib', so_file), extdir)
 
 setup(
     ext_modules=[CMakeExtension('arx_r5_sdk/lib/arx_r5_python', sourcedir='py/ARX_R5_python'), 
